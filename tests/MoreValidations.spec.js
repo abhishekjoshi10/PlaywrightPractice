bb38753b9b048cddbb047e972b04e7fba53fff66
abhishek.joshi10 <abhishek.joshi43@gmail.com>
2026-05-12 21:16:49 +0530

    screenshot test cases added

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -17,3 +17,21 @@ test('PopUp validations', async({page})=>
     
     
 })
+
+test("Screenshot & Visual comparision", async ({page}) =>
+{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await page.screenshot({path: 'screenshot.png'});
+    await expect(page.locator("#displayed-text")).toBeHidden();
+
+});
+
+
+test.only('visual', async ({page}) =>
+{
+    await page.goto("https://www.google.com/");
+    expect(await page.screenshot()).toMatchSnapshot('landing.png');
+
+})
