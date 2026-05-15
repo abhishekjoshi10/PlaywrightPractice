835b373ff79a2099aaefb0129ef4c632b48958e4
abhishek.joshi10 <abhishek.joshi43@gmail.com>
2026-05-15 19:20:16 +0530

    upload Dowload Testcase added

--- tests/NetworkTest.spec.js
+++ tests/NetworkTest.spec.js
@@ -58,19 +58,5 @@ test('Place the Order', async ({ page }) => {
    await page.locator("button[routerlink*='myorders']").click();
    await page.waitForResponse("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*");
    console.log(await page.locator(".mt-4").textContent());
-   // await page.pause();
-   // await page.locator("tbody").waitFor();
-   // const rows = await page.locator("tbody tr");
- 
- 
-   // for (let i = 0; i < await rows.count(); ++i) {
-   //    const rowOrderId = await rows.nth(i).locator("th").textContent();
-   //    if (response.OrderId.includes(rowOrderId)) {
-   //       await rows.nth(i).locator("button").first().click();
-   //       break;
-   //    }
-   // }
-   // const orderIdDetails = await page.locator(".col-text").textContent();
-   // expect(response.OrderId.includes(orderIdDetails)).toBeTruthy();
- 
+   
 });
---
+++ tests/Upload_Download.spec.js
@@ -0,0 +1,64 @@
+const ExcelJs = require('exceljs');
+const { test, expect } = require('@playwright/test');
+
+async function WriteExcelTest(searchText,replaceText,change, filePath) {
+
+    
+    const workbook = new ExcelJs.Workbook();
+    await workbook.xlsx.readFile(filePath)
+    const worksheet = workbook.getWorksheet('Sheet1');
+    const output =await readExcel(worksheet,searchText);
+    const cell = worksheet.getCell(output.row, output.column+change.colChange);
+    cell.value = replaceText;
+    await workbook.xlsx.writeFile(filePath);
+
+}
+
+async function readExcel(worksheet,searchText)
+{
+    let output ={ row:-1, column:-1};
+     worksheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, colNumber) => {
+            // console.log(cell.value);
+
+            if (cell.value === searchText) {
+                console.log("row no :" + rowNumber);
+                console.log("col no :" + colNumber);
+                output.row=rowNumber;
+                output.column=colNumber;
+            }
+
+        })
+
+
+    })
+    return output;
+}
+
+
+//WriteExcelTest("Banana",350,{rowChange:0,colChange:2},"C:/Users/hp/OneDrive/Desktop/playwright projects/ExcelJsUtils/documents/excelTest.xlsx");
+
+test('Upload download excel validation',async ({page})=>
+{
+
+    const textSearch = 'Mango';
+   const updateValue = '350';
+    await page.goto("https://rahulshettyacademy.com/upload-download-test/index.html");
+    const downloadPromise = page.waitForEvent('download');
+
+    await page.getByRole("button", { name: 'Download' }).click();
+
+    const download = await downloadPromise;
+    const filePath = "C:/Users/hp/Downloads/excelTest.xlsx";
+    await download.saveAs(filePath);
+
+    console.log("Download completed and saved to:", filePath);
+
+    WriteExcelTest(textSearch,updateValue,{rowChange:0,colChange:2},filePath);
+    await page.locator("#fileinput").click();
+    await page.locator("#fileinput").setInputFiles(filePath);
+
+
+})
+
+
